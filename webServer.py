18072c16e896224e73fc536078d5c9638160ee84
LTSCodingClub <162832165+LTSCodingClub@users.noreply.github.com>
2024-03-18 21:27:32 +0000

    Update webServer.py
    Note host='0.0.0.0'
    Need to change this to the address of the Pi

--- webServer.py
+++ webServer.py
@@ -180,5 +180,5 @@ def emailSet():
 
 if __name__ == '__main__':
         
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0') # Change host to your Raspberry Pi address
     print("Running")
